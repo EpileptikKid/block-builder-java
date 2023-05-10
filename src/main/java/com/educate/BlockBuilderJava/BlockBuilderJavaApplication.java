a2d1b05c539db64fed0879539da3ed57fb2ff870
Voloskovets Andrii <k1d.ep1lept1k@gmail.com>
2023-05-10 16:23:15 +0300

    created simple project

---
+++ src/main/java/com/educate/BlockBuilderJava/BlockBuilderJavaApplication.java
@@ -0,0 +1,13 @@
+package com.educate.BlockBuilderJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockBuilderJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockBuilderJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educate/BlockBuilderJava/BlockBuilderJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educate.BlockBuilderJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockBuilderJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
